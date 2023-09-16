02adfa9f380e0af3f694a1a66a1499902688c047
Yulia <y.chikaleva99@mail.ru>
2023-09-16 16:34:06 +0300

    Base

--- Example006_Base/Program.cs
+++ Example006_Base/Program.cs
@@ -1,13 +1,18 @@
-﻿int number1 = new Random().Next(1,1000);
+﻿int number1 = new Random().Next(1,10);
 int number2 = new Random().Next(1,10);
 int number3 = new Random().Next(1,10);
 int number4 = new Random().Next(1,10);
 int number5 = new Random().Next(1,10);
 int max = number1;
+int[] weights = {number1, number2, number3, number4, number5};
 
-if (number1 > max) max = number1;
-if (number2 > max) max = number2;
-if (number3 > max) max = number3;
-if (number4 > max) max = number4;
-if (number5 > max) max = number5;
+for (int i=0; i< weights.Length; i++) {
+    Console.Write(weights[i]+ " ");
+}
+Console.WriteLine();
+for (int i=0; i< weights.Length; i++) {
+    if (weights[i] > max) {
+        max = weights[i];
+    }
+}
 Console.WriteLine(max);

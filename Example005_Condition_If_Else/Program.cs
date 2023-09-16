74b6dd096abbb9107cd9a46a2b813a49d81c7e1a
Yulia <y.chikaleva99@mail.ru>
2023-09-16 15:39:02 +0300

    Condition

---
+++ Example005_Condition_If_Else/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите имя");
+string username = Console.ReadLine();
+if (username == "Маша") {
+    Console.Write("Ура, это же Маша!");
+} else {
+    Console.WriteLine("Привет, " + username + "!");
+}

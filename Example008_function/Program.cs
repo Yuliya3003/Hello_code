8dd9c2dc4fe7473608e75119acc7f62e4765aeb0
Yulia <y.chikaleva99@mail.ru>
2023-09-20 10:28:04 +0300

    max

---
+++ Example008_function/Program.cs
@@ -0,0 +1,22 @@
+﻿int Max(int[] ints) {
+    int max = ints[0];
+    for (int i=0; i < ints.Length; i++) {
+        if (max < ints[i]) {
+            max = ints[i];
+        }
+    }
+    return max;
+}
+
+int number1 = new Random().Next(1,10);
+int number2 = new Random().Next(1,10);
+int number3 = new Random().Next(1,10);
+int number4 = new Random().Next(1,10);
+int number5 = new Random().Next(1,10);
+int number6 = new Random().Next(1,10);
+int number7 = new Random().Next(1,10);
+int number8 = new Random().Next(1,10);
+int number9 = new Random().Next(1,10);
+int[] weights = {number1, number2, number3, number4, number5, number6, number7, number8, number9};
+
+Console.WriteLine(Max(weights));

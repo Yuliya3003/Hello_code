a7046aa02d46fc3b771f3f36f986a2e5de80991f
Yulia <y.chikaleva99@mail.ru>
2023-09-20 10:16:52 +0300

    Добавили чисел

--- Example006_Base/Program.cs
+++ Example006_Base/Program.cs
@@ -3,8 +3,12 @@ int number2 = new Random().Next(1,10);
 int number3 = new Random().Next(1,10);
 int number4 = new Random().Next(1,10);
 int number5 = new Random().Next(1,10);
+int number6 = new Random().Next(1,10);
+int number7 = new Random().Next(1,10);
+int number8 = new Random().Next(1,10);
+int number9 = new Random().Next(1,10);
 int max = number1;
-int[] weights = {number1, number2, number3, number4, number5};
+int[] weights = {number1, number2, number3, number4, number5, number6, number7, number8, number9};
 
 for (int i=0; i< weights.Length; i++) {
     Console.Write(weights[i]+ " ");

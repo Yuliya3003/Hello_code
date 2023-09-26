102e9e7b2eacddf89a862d023ab7bebb5da9ccab
Yulia <y.chikaleva99@mail.ru>
2023-09-26 17:22:14 +0300

    New

---
+++ Method_002/Program.cs
@@ -0,0 +1,17 @@
+﻿string Replace(string text, char OldValue, char newValue) {
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i<length; i++) {
+        
+        if (text[i] == (OldValue)) {
+            result += newValue;
+        } else {
+            result += text[i];
+        }
+    }
+    return result;
+}
+
+string text1 = Replace(text:"– Я думаю, – сказал князь, улыбаясь, – что, ежели бы вас послали вместо нашего милого Винценгероде, вы бы взяли приступом согласие прусского короля. Вы так красноречивы. Вы дадите мне чаю?", OldValue:'–', newValue:':');
+string text2 = Replace(text1, 'к', 'К');
+Console.Write(Replace(text2, '?', '!'));

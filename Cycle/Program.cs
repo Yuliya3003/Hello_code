adc541ae283f9646f258ca7c029952b7e566654a
Yulia <y.chikaleva99@mail.ru>
2023-10-01 10:37:41 +0300

    Seminar_4

---
+++ Cycle/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i=1; i <= N; i++) {
+    Console.WriteLine(Math.Pow(i,2));
+}
---
+++ Seminar_4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (А)
+// и выдает сумму чисел от 1 до А
+// int Sum(int A) {
+//     int sum = 0;
+//     for (int i=1; i <= A; i++){
+//        sum = sum + i;
+//     }
+//     return sum;
+// }
+// Console.WriteLine(Sum(4));
+
+// Напишите программу, которая выводит массив из 8 элементов, заполненных
+// 0 и 1
+// int[] GetMassive() {
+//     int[] x = new int[8];
+//     for (int i =0; i < x.Length; i++) {
+//         x[i] = new Random().Next(0,2);
+//     }
+//     return x;
+// }
+// void Getter(int[] ints) {
+//     for (int i =0; i<ints.Length; i++){
+//         Console.Write($"{ints[i]} ");
+//     }
+// }
+// Getter(GetMassive());
